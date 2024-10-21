314de9de9b9503edcf7e7bc66d5e1ff6767649c1
AshrafulMahbeer <sashrafuli27@gmail.com>
2024-10-21 09:41:05 +0600

    Update jago.js

--- api/jago.js
+++ api/jago.js
@@ -3,20 +3,25 @@
 export default function handler(req, res) {
   const { url } = req.query;
 
-  if (url === 'stream.m3u8') {
+  if (url) {
     // Set the response header to serve an m3u8 file
     res.setHeader('Content-Type', 'application/vnd.apple.mpegurl');
 
-    // Generate the master m3u8 with the provided signed URL
-    const signedM3U8 = `
+    // Prepend the domain to the provided URL path
+    const domain = 'https://app24.jagobd.com.bd/';
+    const fullUrl = domain + url;
+
+    // Generate the M3U8 response with the full URL
+    const m3u8Content = `
 #EXTM3U
 #EXT-X-STREAM-INF:BANDWIDTH=1280000
-https://app24.jagobd.com.bd/c3VydmVyX8RpbEU9Mi8xNy8yMDE0GIDU6RgzQ6NTAgdEoaeFzbF92YWxIZTO0U0ezN1IzMyfvcGVMZEJCTEFWeVN3PTOmdFsaWRtaW51aiPhnPTI/somoyt000011226615544544.stream/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTAvMTcvMjAyNCA1OjQyOjQ4IEFNJmhhc2hfdmFsdWU9Q2ZiN0VPWnFEeVBvZ210Q0hQLytyUT09JnZhbGlkbWludXRlcz01MCZpZD01
+${fullUrl}
     `;
 
-    res.status(200).send(signedM3U8);
+    // Send the dynamically generated M3U8 content
+    res.status(200).send(m3u8Content);
   } else {
-    // If the requested URL is invalid, return a 404
-    res.status(404).send('Stream not found');
+    // If the url query parameter is not provided, return a 400 error
+    res.status(400).send('Missing URL parameter');
   }
 }
