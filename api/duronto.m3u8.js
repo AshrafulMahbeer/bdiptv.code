1a67b78704515284b6648410624414a2e6918a3d
AshrafulMahbeer <sashrafuli27@gmail.com>
2025-01-30 09:07:29 +0600

    Update duronto.m3u8.js

--- api/duronto.m3u8.js
+++ api/duronto.m3u8.js
@@ -1,17 +1,9 @@
 export default function handler(req, res) {
-    // Generate random numbers between 1 and 10
-    const getRandomNumber = () => Math.floor(Math.random() * 10) + 1;
-    
+   
     // M3U8 playlist content
     const m3u8Content = `#EXTM3U
 #EXTINF:,
-https://allinonereborn.com/test.m3u8/ts.php?ts=https://starshare.live/live/KVSingh/KVSingh/433.ts?${getRandomNumber()}
-#EXTINF:,
-https://allinonereborn.com/test.m3u8/ts.php?ts=https://starshare.live/live/KVSingh/KVSingh/433.ts?${getRandomNumber()}
-#EXTINF:,
-https://allinonereborn.com/test.m3u8/ts.php?ts=https://starshare.live/live/KVSingh/KVSingh/433.ts?${getRandomNumber()}
-#EXTINF:,
-https://allinonereborn.com/test.m3u8/ts.php?ts=https://starshare.live/live/KVSingh/KVSingh/433.ts?${getRandomNumber()}`;
+https://allinonereborn.com/test.m3u8/ts.php?ts=https://starshare.live/live/KVSingh/KVSingh/433.ts
     
     // Set response headers
     res.setHeader("Content-Type", "application/vnd.apple.mpegurl");
