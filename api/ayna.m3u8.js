6ef14c107aab29c6b5daad7b8ea0739cb8667346
AshrafulMahbeer <sashrafuli27@gmail.com>
2025-11-28 08:50:42 +0600

    Update ayna.m3u8.js

--- api/ayna.m3u8.js
+++ api/ayna.m3u8.js
@@ -1,10 +1,20 @@
 export default async function handler(req, res) {
   try {
-    const response = await fetch("http://ayna-playlist.xfireflix.workers.dev/");
-    const data = await response.text();
+    const response = await fetch("http://ayna-playlist.xfireflix.workers.dev/", {
+      method: "GET",
+      headers: {
+        // VLC default headers
+        "User-Agent": "VLC/3.0.16 LibVLC/3.0.16",
+        "Accept": "*/*",
+        "Connection": "keep-alive",
+        "Icy-MetaData": "1"
+      }
+    });
+
+    const text = await response.text();
 
     res.setHeader("Content-Type", "text/plain");
-    res.status(200).send(data);
+    res.status(200).send(text);
 
   } catch (error) {
     res.status(500).send("Error: " + error.message);
