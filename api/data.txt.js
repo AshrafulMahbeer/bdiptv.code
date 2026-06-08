50572b58a50927c8900f74da89261589ba4a58e3
AshrafulMahbeer <sashrafuli27@gmail.com>
2026-06-08 10:21:00 +0600

    Update data.txt.js

--- api/data.txt.js
+++ api/data.txt.js
@@ -1,5 +1,4 @@
 export default async function handler(req, res) {
-export default async function handler(req, res) {
 
     // ---------------- CORS ----------------
     res.setHeader("Access-Control-Allow-Origin", "*");
